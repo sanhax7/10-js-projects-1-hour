9cf2c6f22c061f6d17c6a13225c17791ed43e9ab
sanhax7 <deepjsan@gmail.com>
2020-12-31 16:08:28 +0530

    Added Auto-Write-Text

---
+++ auto-write-text/script.js
@@ -0,0 +1,13 @@
+const text = "my Viewers on Github are the best!!! <3";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+    index++;
+
+    if(index > text.length){
+        index = 0;
+    }
+}
+setInterval(writeText,100);
